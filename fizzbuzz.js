cc680bf13e9396caad1ab0271fe48ef642e5e824
elukuent <elukuent@gmail.com>
2016-03-30 18:21:57 -0400

    completed script

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,20 @@
 // Place your FizzBuzz code here. 
+
+for (x=1; x < 21; x++) {
+  if (x%3===0 && x%5===0) {
+    console.log("FizzBuzz");
+  } 
+  
+  else if (x%5===0) {
+    console.log("Buzz");
+  }
+  
+  else if (x%3===0) {
+    console.log("Fizz");
+  }
+  
+  else {
+    console.log(x);
+  }  
+}
+
